2652ad05dd24dcf1d29a99ab8390955e10cf374f
Agent Fitz ;-) <fitz.bao@outlook.com>
2024-10-04 22:43:42 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
-#configure.sh VNC_USER_PASSWORD VNC_PASSWORD $CF_TOKEN
+#configure.sh VNC_USER_PASSWORD VNC_PASSWORD $NGROK_AUTH_TOKEN
 
 #disable spotlight indexing
 sudo mdutil -i off -a
@@ -25,9 +25,10 @@ echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-#install cf
-brew install cloudflared
+#install ngrok
+brew install ngrok
 
-# start cf service
-sudo cloudflared service install $3
+#configure ngrok and start it
+ngrok config add-authtoken $3
+ngrok tcp 5900 &
 
